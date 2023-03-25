80d01a3cd61ae5affd0ba1d4b9edf8509051e2c2
Freddy Rueda <frueda@oshyn.com>
2023-03-24 20:00:21 -0500

    Change boxever push method to a js function

--- src/sxastarter/public/cdpSettings.js
+++ src/sxastarter/public/cdpSettings.js
@@ -42,3 +42,33 @@ var _boxeverq = _boxeverq || [];
                 console.log("Copy-paste the following line into Sitecore CDP, Customer Data, Guests, Search field:");
                 console.log(`bid: ${Boxever.getID()}`);
             });
+
+            var callMethod = (page) => 
+            {
+                window.alert('this is the page ' + page);
+                _boxeverq.push(() => {
+                    // Create a "VIEW" event object
+                    const viewEvent = {
+                        browser_id: Boxever.getID(),
+                        channel: "WEB",
+                        type: "VIEW",
+                        language: "EN",
+                        currency: "USD",
+                        page: page,
+                        pos: "pointb.com"
+    
+                    };
+    
+                    // Send the event data to the server
+                    Boxever.eventCreate(
+                        viewEvent,
+                        () => {},
+                        "json"
+                    );
+    
+                    // Log the browser ID to the console
+                    console.log("Copy-paste the following line into Sitecore CDP, Customer Data, Guests, Search field:");
+                    console.log(`bid: ${Boxever.getID()}`);
+                });
+
+            }
